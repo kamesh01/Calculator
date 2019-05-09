0e391ef8ee6416f57d0eb4c4c749446e465ab9bd
KAMESH-PC <KAMESH-PC@DESKTOP-KLR143>
2019-05-09 22:10:21 +0530

    successfully completed

--- src/main/java/main/Calculator.java
+++ src/main/java/main/Calculator.java
@@ -9,10 +9,10 @@ public class Calculator {
         int num1,num2;
         num1 = inp.nextInt();
         num2 = inp.nextInt();
-        int ans;
         System.out.println("Enter your selection: 1 for Addition, 2 for substraction 3 for Multiplication and 4 for division:");
         int choose;
         choose = inp.nextInt();
+        inp.close();
         switch (choose){
         case 1:
             System.out.println(add( num1,num2));
@@ -31,9 +31,7 @@ public class Calculator {
 
 
         }
-
-
-
+        
     }
     public static int add(int x, int y)
     {
